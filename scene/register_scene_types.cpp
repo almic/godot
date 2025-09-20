38a50390801c16bbfd9cc59af94fa4646534dd25
almic <mick.ashton@flare-esports.net>
2025-09-19 18:37:06 -0600

    Initial proof of concept for JoltVehicle
    - Added wheel settings resource which just interfaces with
      the jolt wheel settings struct
    - Modified build scripts to support this

--- scene/register_scene_types.cpp
+++ scene/register_scene_types.cpp
@@ -268,6 +268,7 @@
 #include "scene/3d/physics/joints/joint_3d.h"
 #include "scene/3d/physics/joints/pin_joint_3d.h"
 #include "scene/3d/physics/joints/slider_joint_3d.h"
+#include "scene/3d/physics/jolt_vehicle/wheel_base_settings.h"
 #include "scene/3d/physics/kinematic_collision_3d.h"
 #include "scene/3d/physics/physical_bone_3d.h"
 #include "scene/3d/physics/physics_body_3d.h"
@@ -618,6 +619,8 @@ void register_scene_types() {
 	GDREGISTER_CLASS(KinematicCollision3D);
 	GDREGISTER_CLASS(CharacterBody3D);
 	GDREGISTER_CLASS(SpringArm3D);
+
+	GDREGISTER_CLASS(WheelBaseSettings);
 
 	GDREGISTER_CLASS(PhysicalBoneSimulator3D);
 	GDREGISTER_CLASS(PhysicalBone3D);

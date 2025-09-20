39dd809d0a98811daf7f30e34893d3869b9456ae
almic <mick.ashton@flare-esports.net>
2025-09-20 12:07:38 -0600

    Add differential, engine, transmission settings
    - Added VehicleDifferentialSettings
    - Added VehicleEngineSettings
    - Started on VehicleTransmissionSettings, incomplete
    - Added type conversion for Jolt LinearCurve to/ from
      Godot's Curve

---
+++ scene/3d/physics/jolt_vehicle/vehicle_engine_settings.h
@@ -0,0 +1,80 @@
+#pragma once
+
+#include "core/io/resource.h"
+#include "scene/resources/curve.h"
+
+#include "Jolt/Jolt.h"
+#include "Jolt/Physics/Vehicle/VehicleEngine.h"
+
+class VehicleEngineSettings : public Resource {
+	GDCLASS(VehicleEngineSettings, Resource);
+
+protected:
+	JPH::VehicleEngineSettings settings;
+
+public:
+
+	VehicleEngineSettings()
+	{
+		_read_torque_curve();
+	}
+
+	real_t get_max_torque() const { return (real_t) settings.mMaxTorque; }
+	void set_max_torque(real_t p_max_torque) { settings.mMaxTorque = (float) settings.mMaxTorque; }
+
+	real_t get_min_rpm() const { return (real_t) settings.mMinRPM; }
+	void set_min_rpm(real_t p_min_rpm) { settings.mMinRPM = (float) p_min_rpm; }
+
+	real_t get_max_rpm() const { return (real_t) settings.mMaxRPM; }
+	void set_max_rpm(real_t p_max_rpm) { settings.mMaxRPM = (float) p_max_rpm; }
+
+	real_t get_inertia() const { return (real_t) settings.mInertia; }
+	void set_inertia(real_t p_inertia) { settings.mInertia = (float) p_inertia; }
+
+	real_t get_angular_damping() const { return (real_t) settings.mAngularDamping; }
+	void set_angular_damping(real_t p_angular_damping) { settings.mAngularDamping = (float) p_angular_damping; }
+
+	Ref<Curve> get_normalized_torque_curve() const
+	{
+		return torque_curve;
+	}
+	void set_normalized_torque_curve(const Ref<Curve>& p_torque_curve)
+	{
+		if (torque_curve.is_valid()) {
+			torque_curve->disconnect_changed(callable_mp(this, &VehicleEngineSettings::_apply_torque_curve));
+		}
+
+		torque_curve = p_torque_curve;
+
+		if (torque_curve.is_valid()) {
+			torque_curve->connect_changed(callable_mp(this, &VehicleEngineSettings::_apply_torque_curve));
+			_apply_torque_curve();
+		}
+		else
+		{
+			// Reset to default
+			JPH::VehicleEngineSettings defaults;
+			settings.mNormalizedTorque = defaults.mNormalizedTorque;
+			_read_torque_curve();
+		}
+	}
+
+protected:
+	Ref<Curve> torque_curve;
+
+	void _apply_torque_curve() const {
+		settings.mNormalizedTorque = to_jolt(*torque_curve);
+	}
+
+	void _read_torque_curve() {
+		if (torque_curve.is_valid()) {
+			torque_curve->disconnect_changed(callable_mp(this, &VehicleEngineSettings::_apply_torque_curve));
+		}
+
+		torque_curve = Ref<Curve>(&to_godot(settings.mNormalizedTorque));
+		torque_curve->connect_changed(callable_mp(this, &VehicleEngineSettings::_apply_torque_curve));
+	}
+
+	static void _bind_methods();
+};
+

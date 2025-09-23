2b773ff332ece8b5dd0983bde6cd3dea25d5d103
almic <mick.ashton@flare-esports.net>
2025-09-23 11:30:38 -0600

    Update run_build.sh, skip copy if failed

--- run_build.sh
+++ run_build.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-scons production=true precision=double
+scons production=true
 
-rm '/home/almic/.local/share/Steam/steamapps/common/Godot Engine/godot.x11.opt.tools.64'
-ln '/home/almic/repos/godot/bin/godot.linuxbsd.editor.double.x86_64' '/home/almic/.local/share/Steam/steamapps/common/Godot Engine/godot.x11.opt.tools.64'
+retVal=$?
+if [ $retVal -ne 0 ]; then
+    echo "Error during build, skipping output build copy"
+	exit $retval
+fi
+
+#rm '/home/almic/.local/share/Steam/steamapps/common/Godot Engine/godot.x11.opt.tools.64'
+#ln '/home/almic/repos/godot/bin/godot.linuxbsd.editor.double.x86_64' '/home/almic/.local/share/Steam/steamapps/common/Godot Engine/godot.x11.opt.tools.64'
 

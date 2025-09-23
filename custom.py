06db23352d777edbf39534494b20168f3bf8a747
almic <mick.ashton@flare-esports.net>
2025-09-23 11:29:33 -0600

    Add custom.py for building

---
+++ custom.py
@@ -0,0 +1,23 @@
+# Generated using https://godot-build-options-generator.github.io
+
+extra_suffix = "zombie_game"
+production = "yes"
+precision = "double"
+use_llvm = "yes"
+module_camera_enabled = "no"
+module_dds_enabled = "no"
+module_enet_enabled = "no"
+module_gridmap_enabled = "no"
+module_jsonrpc_enabled = "no"
+module_ktx_enabled = "no"
+module_mbedtls_enabled = "no"
+module_mobile_vr_enabled = "no"
+module_multiplayer_enabled = "no"
+module_openxr_enabled = "no"
+#module_regex_enabled = "no"
+module_tga_enabled = "no"
+module_upnp_enabled = "no"
+module_webrtc_enabled = "no"
+module_websocket_enabled = "no"
+module_webxr_enabled = "no"
+
